606f0b89deb3f5647ba5883141916fb72b8a2d7a
Paul de Nonancourt <paul.de.nonancourt@gmail.com>
2022-12-01 08:41:50 +0100

    [day01] Part 1 & part 2 solved

--- lib/advent_of_code/day01.ex
+++ lib/advent_of_code/day01.ex
@@ -1,10 +1,27 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_) do
+  def part1(args) do
+    sum_backpacks_calories(args)
+    |> Enum.max()
   end
 
-  def part2(_) do
+  def part2(args) do
+    sum_backpacks_calories(args)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
   end
 
-  defp input do
+  defp sum_backpacks_calories(args) do
+    backpacks =
+      args
+      |> String.split("\n")
+      |> Enum.chunk_by(fn line -> line != "" end)
+      |> Enum.reject(fn line -> line == [""] end)
+
+    for backpack <- backpacks do
+      backpack
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end
   end
 end
